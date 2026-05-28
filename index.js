3403eabe8ec0eaeff6d9bbeda278061aeb7807db
riceel527-hash <riceel527@gmail.com>
2026-05-28 13:20:13 -0400

    added css

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
-console.log(fetch("https://jsonplaceholder.typicode.com/users"));
+async function main() {
+ console.log((await fetch("https://jsonplaceholder.typicode.com/users")).json()); 
+} 
+   
+main();
