a41e271c5d5fc99902d5e2a6b4709cc88599fab4
riceel527-hash <riceel527@gmail.com>
2026-05-29 09:45:46 -0400

    added css

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ async function main () {
  const users = await fetch("https://jsonplaceholder.typicode.com/users");
  const usersData = await users.json();
  const userListEl = document.querySelector(".user-list");
+ console.log(usersData);
 
  userListEl.innerHTML = usersData.map(
-    (user) =>`<div class="user-card">
-    <div class="user-card__container">
-      <h3>User's Name</h4>
-        <p><b>Email:</b> email@email.com</p>
-        <p><b>Phone:</b> 0000000000</p>
-        <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+    (user) =>
+        `<div class="user-card">
+         <div class="user-card__container">
+          <h3>${user.name}</h3>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
     </div>
     </div>`
+
+
+
           )
           .join("");
 }
