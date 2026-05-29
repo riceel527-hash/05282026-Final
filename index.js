0d5d1bbcc2bc0aada427bbb44c926d046935cfe0
riceel527-hash <riceel527@gmail.com>
2026-05-29 10:58:31 -0400

    added css

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
 
 async function main () {
  const users = await fetch("https://jsonplaceholder.typicode.com/users");
  const usersData = await users.json();
- const userListEl = document.querySelector(".user-list");
- console.log(usersData);
  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
 main ();
 
+function showUserPosts(id) {
+    window.location.href = `${window.location.origin}/user.html`;
+  
+}
+
 function userHTML (user) {
- return `<div class="user-card">
+ return `<div class="user-card" onclick="showUserPosts(${user.id})">
          <div class="user-card__container">
           <h3>${user.name}</h3>
         <p><b>Email:</b> ${user.email}</p>
@@ -21,6 +25,8 @@ function userHTML (user) {
     </div>
     </div>`;
 }
+
+
 
 
 
